3ac7a7d9ca41ccdb6bfa53dc17e35cbdae55d37c
Nicola <nicolavirgilio29@gmail.com>
2025-10-31 17:50:19 +0100

    Cors conf

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+  next()
+})
 
 app.use(express.json())
 
