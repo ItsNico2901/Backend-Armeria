ae189cd9b631f1d0c8d092c097796a4bb7b4494b
Nicola <nicolavirgilio29@gmail.com>
2025-10-31 01:09:23 +0100

    CORS

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ import userRoutes from './routes/users.routes.js'
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: true, // o reemplaza por tu origen: 'http://localhost:5173'
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-role', 'X-Requested-With'],
+    credentials: false,
+  })
+)
 app.use(express.json())
 
 app.use('/api', authRoutes)
